e8a803f5679fabc728f350cab9e2b61bca3f2b0e
cindydhy <38956884+cindydhy@users.noreply.github.com>
2025-07-09 21:07:50 -0700

    Add files via upload

---
+++ src/test/java/GuessingGameTest.java
@@ -0,0 +1,39 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class GuessingGameTest {
+
+    // @Test(timeout = 1000)
+    // public void testGuessingGameGetGuess1() {
+
+    //     String secret = "decided";
+    //     String guess = "jacuzzi";
+    //     String expected = "|_|_|^|_|_|_|*|";
+
+    //     String message = "Check GuessingGame getGuess()";
+    //     assertEquals(message,  expected, GuessingGame.getGuess(secret, guess));
+    // }
+
+    // @Test(timeout = 1000)
+    // public void testGuessingGameGetGuess2() {
+
+    //     String secret = "decided";
+    //     String guess = "sharply";
+    //     String expected = "|_|_|_|_|_|_|_|";
+
+    //     String message = "Check GuessingGame getGuess()";
+    //     assertEquals(message,  expected, GuessingGame.getGuess(secret, guess));
+    // }
+
+    // @Test(timeout = 1000)
+    // public void testGuessingGameGetGuess3() {
+
+    //     String secret = "decided";
+    //     String guess = "decided";
+    //     String expected = "You win! The word is decided";
+
+    //     String message = "Check GuessingGame getGuess()";
+    //     assertEquals(message,  expected, GuessingGame.getGuess(secret, guess));
+    // }
+    
+}
